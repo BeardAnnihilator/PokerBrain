2c57290ccce490af8408559ecd3f0878a918ab8e
Alexandre <periphanos.a@gmail.com>
2014-07-17 12:15:20 +0200

    Init

---
+++ PokerBrain/PokerBrain/Models/PokerBrainEntities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PokerBrain.Models
+{
+    public class PokerBrainEntities : DbContext
+    {
+        public PokerBrainEntities()
+            : base("PokerBrainEntities")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ PokerBrain/PokerBrain/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PokerBrain.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
