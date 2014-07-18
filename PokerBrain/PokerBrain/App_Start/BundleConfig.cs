dd8820098bc689c34219c3a6af1e714b67cf1999
Alexandre <periphanos.a@gmail.com>
2014-07-18 13:57:50 +0200

    New: home design

--- PokerBrain/PokerBrain/App_Start/BundleConfig.cs
+++ PokerBrain/PokerBrain/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace PokerBrain
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/css/shared.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
